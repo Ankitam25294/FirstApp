7729d50524b2384e8a121fadbb3a2f723a0a184a
Ankitam25294 <40385114+Ankitam25294@users.noreply.github.com>
2018-06-22 09:02:00 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`<div style="text-align:center">  
+  <div style="color:blue">
+    <h1>
+      Marvellous Infosystems
+    </h1>
+    <h2>
+      Educating for better tomorrow.
+    </h2>
+  </div>
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+</div>`,  //inline
+
+//templateUrl: ['./app.component.html']
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Marvellous Infosystems';
 }
